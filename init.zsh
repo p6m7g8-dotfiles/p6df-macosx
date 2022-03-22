aa8d8a27918423aa62eeb234d61097a6fce3daae
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-22 08:51:03 -0400

    ci: testing (#5)

--- init.zsh
+++ init.zsh
@@ -18,42 +18,6 @@ p6df::modules::macosx::deps() {
 #
 #>
 ######################################################################
-akamai: CLI toolkit for working with Akamai's APIs'
-angular-cli: CLI tool for Angular
-athenacli: CLI tool for AWS Athena service
-autorestic: High level CLI utility for restic
-
-azure-cli: Microsoft Azure CLI 2.0
-
-bosh-cli: Cloud Foundry BOSH CLI v2
-
-cloudflare-wrangler: CLI tool for Cloudflare Workers
-cloudflared: Cloudflare Tunnel client (formerly Argo Tunnel)
-
-drone-cli: Command-line client for the Drone continuous integration server
-
-athenacli: CLI tool for AWS Athena service
-aws-cfn-tools: Client for Amazon CloudFormation web service
-aws-console: Command-line to use AWS CLI credentials to launch the AWS console in a browser
-aws-elasticbeanstalk: Client for Amazon Elastic Beanstalk web service
-aws-es-proxy: Small proxy between HTTP client and AWS Elasticsearch
-aws-shell: Integrated shell for working with the AWS CLI
-aws-sam-cli: AWS SAM CLI 🐿 is a tool for local development and testing of Serverless applications
-aws-sam-cli-beta-cdk: AWS SAM CLI 🐿 is a tool for local development and testing of Serverless applications. This is a pre-release version of AWS SAM CLI
-aws-sam-cli-nightly: AWS SAM CLI 🐿 is a tool for local development and testing of Serverless applications. This is a pre-release version of AWS SAM CLI
-aws-sam-cli-rc: AWS SAM CLI 🐿 is a tool for local development and testing of Serverless applications
-aws-simple-ec2-cli: AWS Simple EC2 CLI is a tool that simplifies the process of launching, connecting and terminating an EC2 instance
-copilot-cli: Copilot CLI - build, release and operate your container apps on AWS
-awsweeper: CLI tool for cleaning your AWS account
-chamber: CLI for managing secrets through AWS SSM Parameter Store
-copilot: CLI tool for Amazon ECS and AWS Fargate
-fargatecli: CLI for AWS Fargate
-gimme-aws-creds: CLI to retrieve AWS credentials from Okta
-awless: The Mighty CLI for AWS
-aws-vpn-client: AWS Client VPN
-session-manager-plugin: Session Manager Plugin for the AWS CLI
-elasticwolf: AWS ElasticWolf Client Console
-
 p6df::modules::macosx::external::brew() {
 
   brew tap homebrew/cask
